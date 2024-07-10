44fa32da875ac86688a2074a8d65a75a210173e1
RiversideRocks <wilest7@gmail.com>
2024-07-10 10:57:15 -0400

    basic book scraper

---
+++ htmlparse/index.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const BASE_URL = "https://books.toscrape.com/"
+
+const req = axios.create({
+    headers: {
+        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0'
+    },
+    baseURL: BASE_URL
+});
+
+async function extractBooks(){
+    const raw = await req.get()
+    const $ = cheerio.load(raw.data)
+
+    $('li.col-xs-6').each((index, element) => {
+        var x = cheerio.load(element)
+        console.log(x('a').text())
+        console.log(BASE_URL + x('a').attr('href'))
+    })
+}
+
+extractBooks()

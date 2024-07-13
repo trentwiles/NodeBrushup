86090125b3db72eeebf366a48c83789bc390179a
trentwiles <wilest7@gmail.com>
2024-07-12 23:18:35 -0400

    basic scrape

--- email/index.js
+++ email/index.js
@@ -1,16 +1,39 @@
 const send = require('./send')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const BASE_URL = 'https://www.motorsport.com'
 
 const req = axios.create({
     headers: {
         'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0'
     },
-    baseURL: 'https://www.motorsport.com/f1/news/'
+    baseURL: BASE_URL
 });
 
 async function getNews(){
-    const raw = await req.get()
-    console.log(raw.data)
+    const raw = await req.get('/f1/news/')
+    const html = raw.data
+    const $ = cheerio.load(html)
+
+    var newsItems = []
+
+    $('a.ms-item').each((index, element) => {
+        const x = cheerio.load($(element).html())
+
+        var title = x('div.ms-item__title').text()
+        var url = BASE_URL + $(element).attr('href')
+        var time = x('time.ms-item__date').text().trim()
+        var image = x('img').attr('src')
+        var metaData = {"title": title, "url": url, "time": time, 'image': image}
+        console.log(metaData)
+        newsItems.push(metaData)
+
+        var statement = (newsItems.length > 2)
+        console.log(statement)
+        if(statement){
+            return newsItems
+        }
+    })
 }
+getNews()
 // send.send('Frank', 'me@trentwil.es', 'A New Email', 'test', '<i>test</i>')
